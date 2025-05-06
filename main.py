ce01c8b096ac79d24555cea5a6b5a770d2495397
ahmetemresavaseri <ahmetemresavaseri@gmail.com>
2025-05-06 20:21:27 +0200

    imports

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+
+mnist = tf.keras.datasets.mnist # Load the MNIST dataset of handwritten digits
+
+
+
+
+
