1c24f5e79887b620ce980368717711b6d1a947b7
ahmetemresavaseri <ahmetemresavaseri@gmail.com>
2025-05-06 20:26:35 +0200

    first code

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ import tensorflow as tf
 
 mnist = tf.keras.datasets.mnist # Load the MNIST dataset of handwritten digits
 
-
+(x_train, y_train), (x_test, y_test) = mnist.load_data() # Split the dataset into training and testing sets
+x_train = x_train / 255.0 # Normalize the training data to the range [0, 1]
 
 
 
